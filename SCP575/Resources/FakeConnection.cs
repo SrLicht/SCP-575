bb936cd8425e2e141f8928d0b73ad5c3dafd7d2e
SrLicht <francoquiroz_13@live.com>
2023-06-12 20:43:01 -0300

    Update FakeConnection.cs
    * Update to use Mirror of SCP:SL 13.0

--- SCP575/Resources/FakeConnection.cs
+++ SCP575/Resources/FakeConnection.cs
@@ -1,18 +1,36 @@
 using System;
+using GameCore;
 using Mirror;
+using PluginAPI.Core;
 
 namespace SCP575.Resources
 {
     public class FakeConnection : NetworkConnectionToClient
     {
-        public override void Send(ArraySegment<byte> segment, int channelId = 0)
+
+        public FakeConnection(int networkConnectionId) : base(networkConnectionId)
         {
         }
 
-        public override string address => "localhost";
-        
-        public FakeConnection(int networkConnectionId) : base(networkConnectionId, false, 0)
+        public override string address => "YourMom";
+
+        public override void Send(ArraySegment<byte> segment, int channelId = 0)
         {
+            // Ignore
+        }
+
+        public override void Disconnect()
+        {
+            PluginAPI.Core.Log.Debug("Destroying Dummy", Scp575.Instance.Config.Debug);
+
+            try
+            {
+                Dummies.DestroyDummy(identity.gameObject.GetComponent<ReferenceHub>());
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
         }
     }
 }
